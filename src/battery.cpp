48f1ffe1b748e0ef08e24bcf5dc82cb05555a7ef
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-06-24 12:10:03 +0900

    Update battery.cpp

--- src/battery.cpp
+++ src/battery.cpp
@@ -9,15 +9,16 @@
 class BATTERY_CHECK{
 	public:
 		ros::NodeHandle n;
-		ros::NodeHandle pnh("~");
-		pnh.param("threshold", threshold, default_threshold);
 		void batteryCallback(const limo_base::LimoStatus& msg);
 		ros::Subscriber sub = n.subscribe("limo_status", 10, &BATTERY_CHECK::batteryCallback, this);
 		ros::Publisher charge_pub = n.advertise<std_msgs::Bool>("charge",1000);
 	
 	
 	
-		BATTERY_CHECK(){}
+		BATTERY_CHECK(){
+			ros::NodeHandle pnh("~");
+			pnh.param("threshold", threshold, default_threshold);
+		}
 		~BATTERY_CHECK(){}
 	private:
 		float threshold;
