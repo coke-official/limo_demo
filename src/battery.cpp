d7136265a98fb8b43f847d21894cd1674c258c8c
Wataru Oshima <woshima@mail.csuchico.edu>
2022-08-05 08:58:06 +0800

    updated a battery_cp

--- src/battery.cpp
+++ src/battery.cpp
@@ -1,31 +1,62 @@
 #include "unistd.h"
 #include "ros/ros.h"
+#include "ros/package.h"
 #include "std_msgs/Header.h"
+#include "std_msgs/Bool.h"
 #include "sound_play/sound_play.h"
-#include "limo_base/LimoStatus.h"
+#include "limo_base/LimoStatus.h" 
 
-float threshold;
+class BATTERY_CHECK{
+	public:
+		ros::NodeHandle n;
+		void batteryCallback(const limo_base::LimoStatus& msg);
+		ros::Subscriber sub = n.subscribe("limo_status", 10, &BATTERY_CHECK::batteryCallback, this);
+		ros::Publisher charge_pub = n.advertise<std_msgs::Bool>("charge",1000);
+		ros::Rate rate_;
+		
+	
+	
+		BATTERY_CHECK() :
+			rate_(0.3)
+		{
+			ros::NodeHandle pnh("~");
+			pnh.param("threshold", threshold, default_threshold);
+		}
+		~BATTERY_CHECK(){}
+	private:
+		float threshold;
+		float default_threshold =12;
+		std::vector<int> charging_threshold;
+};
 
-void batteryCallback(const limo_base::LimoStatus& msg)
+void BATTERY_CHECK::batteryCallback(const limo_base::LimoStatus& msg)
 {
+	std::string path = "/home/agilex/catkin_ws/src/limo_demo/voice/juuden.wav";
+	std_msgs::Bool charge;
 	sound_play::SoundClient sound_client;
 	float voltage = msg.battery_voltage;
 	ROS_INFO("battery_voltage: %f", voltage);
 	if (voltage < threshold)
 	{
-		sound_client.playWave("/home/agilex/agilex_ws/src/limo_ros/limo_demo/voice/juuden.wav", 1.0);
-		sleep(3);
+		charging_threshold.push_back(1);
+	}else{
+		charging_threshold.clear();
+		charge.data = false;
 	}
+
+	if(charging_threshold.size()>20){
+		sound_client.playWave(path, 1.0);
+		charge.data = true;
+	}
+	charge_pub.publish(charge);
+	rate_.sleep();
 }
+
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "battery");
-	sound_play::SoundClient sound_client;
-	ros::NodeHandle n;
-	ros::NodeHandle pnh("~");
-	pnh.getParam("threshold", threshold);
-	ros::Subscriber sub = n.subscribe("limo_status", 10, batteryCallback);
+	BATTERY_CHECK btc;
 	ros::spin();
 
 	return 0;
